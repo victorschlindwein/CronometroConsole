fe01d87cf5ed40cda29ca35e8f85b2773e65ea87
dizzysch <victorwilbert@gmail.com>
2023-09-15 11:43:16 -0300

    refatorando pra melhorar o codigo

--- Program.cs
+++ Program.cs
@@ -5,23 +5,25 @@ Menu();
 static void Menu()
 {
   Console.Clear();
-
   Console.WriteLine("Escolha  o modo que quer cronometrar");
   Console.WriteLine("1 => Cronometro progressivo");
   Console.WriteLine("2 => Cronometro regressivo");
+  Console.WriteLine("0 = Sair");
 
   int stopwatchType = int.Parse(Console.ReadLine());
-  Console.WriteLine(stopwatchType);
-  Console.Clear();
 
+  if (stopwatchType == 0)
+  {
+    System.Environment.Exit(0);
+  }
+
+  Console.Clear();
   Console.WriteLine("S = Segundos => 10s = 10 segundos");
   Console.WriteLine("M = Minutos => 1m = 1 minuto");
   Console.WriteLine("0 = Sair");
   Console.WriteLine("Quanto tempo deseja contar?");
 
-
   string data = Console.ReadLine().ToLower();
-
   char type = char.Parse(data.Substring(data.Length - 1, 1));
   int time = int.Parse(data.Substring(0, data.Length - 1));
   int multiplier = 1;
@@ -36,19 +38,12 @@ static void Menu()
     System.Environment.Exit(0);
   }
 
-  switch (stopwatchType)
-  {
-    case 1:
-      PreStartProgressive(time * multiplier);
-      break;
-    case 2:
-      PreStartRegressive(time * multiplier);
-      break;
-  }
+  int timer = time * multiplier;
+
+  PreStart(timer, stopwatchType);
 }
 
-static void PreStartProgressive(int time)
-
+static void PreStart(int timer, int stopwatchType)
 {
   Console.Clear();
   Console.WriteLine("Ready...");
@@ -57,7 +52,16 @@ static void PreStartProgressive(int time)
   Thread.Sleep(500);
   Console.WriteLine("Go...");
   Thread.Sleep(500);
-  StartProgressive(time);
+
+  switch (stopwatchType)
+  {
+    case 1:
+      StartProgressive(timer);
+      break;
+    case 2:
+      StartRegressive(timer);
+      break;
+  }
 }
 
 static void StartProgressive(int time)
@@ -77,18 +81,6 @@ static void StartProgressive(int time)
   Thread.Sleep(1500);
 
   Menu();
-}
-
-static void PreStartRegressive(int time)
-{
-  Console.Clear();
-  Console.WriteLine("Go...");
-  Thread.Sleep(500);
-  Console.WriteLine("Set...");
-  Thread.Sleep(500);
-  Console.WriteLine("Ready...");
-  Thread.Sleep(500);
-  StartRegressive(time);
 }
 
 static void StartRegressive(int time)
